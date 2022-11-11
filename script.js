d2e6519f68248c5344fd11040c26526f8c969c24
Mirboboyev Jasur <mirboboyevjasurjan@gmail.com>
2022-11-11 22:48:44 +0500

    11.11.2022

--- script.js
+++ script.js
@@ -4,44 +4,74 @@ const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const context = document.getElementById("context")
 // Images
-import image from "./assets/apple-man.png"
+// const "image" = require("./assets/apple-man.png");
 
-
-cards.forEach((card, i) => {
-  const cardProp = card.getBoundingClientRect();
-  const cardWidth = cardProp.width
-
-  nextBtn.addEventListener('click', ()=>{
-   card.scrollLeft += cardWidth;
-  })
-  prevBtn.addEventListener('click', ()=>{
-    card.scrollLeft -= cardWidth;
-  })
-})
-
+document.innerHTML = "Hello world"
+document.innerHTML = "image"
+console.log(200)
 persons = [
-  ["Alan Jons", "Lorem Ipsum", image],
-  ["John Doe", "Lorem Ipsum", image],
-  ["Spider Man", "Spider-Man", image],
-  ["Iron Man", "Batman", image],
+{
+  name: "Alan Jons", 
+  text: "Lorem Ipsum",  
+  img: "image"
+},
+{
+  name: "John Doe", 
+  text: "Lorem Ipsum",    
+  img: "image"
+},
+{
+  name: "Spider Man", 
+  text: "Spider-Man",  
+  img: "image"
+},
+{
+  name: "Iron Man", 
+  text:"Batman",  
+  img: "image"
+},
 ]
 
-const changeContainers = (arr) => {
-  arr.map((e,i) => {
-    h3 = document.createElement('h3')
-    p = document.createElement('p')
-    div = document.createElement('div')
-    img = document.createElement('img')
 
-    div.appendChild(h3)
-    div.appendChild(p)
-    div.appendChild(img)
-
-
+persons.map((e,i) => {
+    nextBtn.addEventListener("click", ()=>{
+      if(i != persons.length){
+        i+=1
+      }else{
+        i = 0
+      }
+    })
+      prevBtn.addEventListener("click", ()=>{
+        if(i != 0 ){
+          i-=1
+        }else{
+          i = persons.length
+        }
+      })
+  let div = document.createElement('div')        
+  div.classList.add('context')
+  div.innerHTML = `
+      <div class="per75">
+      <div class="contextText">
+      <h3 class="contextName">${e.name}</h3>
+      <p>${e.text}</p>
+      </div>
+        <img class="contextImage" src="${e.img}" alt="Present Person">
+        </div>
+        <div class="per25">
+        <img class="contextNP" src=${e.img} alt="Next person">
+        <div class="contextBtns">
+          <button class="contextBtn contextBtnPrev" id="contextPrev"> <- Предыдущий </button>
+          <button class="contextBtn contextBtnNext" id="contextNext"> Next-> </button>
+        </div>
+      </div>
+  `;
+        context.appendChild(div)
+        console.log(persons)
   }  )
-}
 
-changeContainers(persons)
+  console.log(200)
+
 
 
 //     // write data to  pool
@@ -58,3 +88,22 @@ changeContainers(persons)
 //     span.appendChild(italic);
 //     taxi.appendChild(span);
 //     road.appendChild(taxi);
+
+
+
+
+
+
+
+
+// cards.forEach((card, i) => {
+//   const cardProp = card.getBoundingClientRect();
+//   const cardWidth = cardProp.width
+
+//   nextBtn.addEventListener('click', ()=>{
+//    card.scrollLeft += cardWidth;
+//   })
+//   prevBtn.addEventListener('click', ()=>{
+//     card.scrollLeft -= cardWidth;
+//   })
+// })
